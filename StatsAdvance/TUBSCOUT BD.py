006804e3978aabe54ea11ce25ece8637a9aca7b7
fblasco1 <francisco.o.blasco@gmail.com>
2020-04-23 17:10:36 -0300

    Araña integrada

--- StatsAdvance/TUBSCOUT BD.py
+++ StatsAdvance/TUBSCOUT BD.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StatsAdvance.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StatsAdvance.settings.local")
 import django
 from selenium import webdriver 
 import pandas as pd
