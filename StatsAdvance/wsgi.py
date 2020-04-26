901d6d4d36fcaaaa5b0ad6616ec71ff10e552863
fblasco1 <francisco.o.blasco@gmail.com>
2020-04-26 01:53:49 -0300

    wsgi corregido

--- StatsAdvance/wsgi.py
+++ StatsAdvance/wsgi.py
@@ -6,9 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
