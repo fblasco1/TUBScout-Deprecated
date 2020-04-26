a887bb4a7a0b16406a04a8f39337492a9e88c744
fblasco1 <francisco.o.blasco@gmail.com>
2020-04-26 01:32:10 -0300

    Correct structure

--- StatsAdvance/StatsAdvance/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect 
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView
-
-class LandingPage(TemplateView):
-    template_name = 'index.html'
--- StatsAdvance/scraper/scraper/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class ScraperConfig(AppConfig):
-    name = 'scraper'
