c48da3f4f6e089ff4a92d4058085d2528a1b2360
fblasco1 <francisco.o.blasco@gmail.com>
2020-04-26 00:42:46 -0300

    Settings Heroku

--- StatsAdvance/StatsAdvance/settings/base.py
+++ StatsAdvance/StatsAdvance/settings/base.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'StatsAdvance.urls'
@@ -126,6 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (BASE_DIR,'static')
@@ -133,5 +135,7 @@ STATICFILES_DIRS = (BASE_DIR,'static')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
